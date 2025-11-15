064ce73cef0e0ee8f506a7989aa7dc0d7346abfc
MohamedNawsany <dnawasany@gmail.com>
2025-11-15 02:43:24 +0200

    hero

--- app/Hero-DomeGallery/DomeGallery.js
+++ app/Hero-DomeGallery/DomeGallery.js
@@ -8,7 +8,7 @@ const DEFAULT_IMAGES = [
     alt: 'Abstract art'
   },
   {
-    src: 'https://images.unsplash.com/photo-1755569309049-98410b94f66d?q=80&w=772&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    src: '/p-hero.jpg',
     alt: 'Modern sculpture'
   },
   {
